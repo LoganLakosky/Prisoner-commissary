f81f9c5db012cf93b62a2fc792f397b9a1136e7b
Logan <59356357+LoganLakosky@users.noreply.github.com>
2023-09-05 14:42:38 -0500

    Setup more basics

--- src/app/mainPageContent.tsx
+++ src/app/mainPageContent.tsx
@@ -5,7 +5,8 @@ export default function MainPageContent(){
   return (
     <div className="mainPageContentContainer">
       <div className="mainPageContent">
-        
+        <div className="productsContainer1"></div>
+        <div className="productsContainer2"></div>
       </div>
     </div>
   )
